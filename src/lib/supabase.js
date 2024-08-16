2e371ae5421861f1ecf954d1479b9857bcd589f3
Eduardo Moraes <adomoraes@gmail.com>
2024-08-16 14:53:49 -0300

    feat: atualizar chave supabase

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,5 +3,5 @@ import { createClient } from "@supabase/supabase-js"
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(
 	"https://sgklpulswavrdvbtuhea.supabase.co",
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNna2xwdWxzd2F2cmR2YnR1aGVhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxNTI2MzA5OSwiZXhwIjoyMDMwODM5MDk5fQ.XjD1ZxLG8nKBM33dxyaX_GHgY-M24CLF8L0hl8PhNKg"
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNna2xwdWxzd2F2cmR2YnR1aGVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUyNjMwOTksImV4cCI6MjAzMDgzOTA5OX0.irWdD3a-rs3CyFyAC-aZMClEWM7LyWVvlsOBZDoD-PY"
 )
